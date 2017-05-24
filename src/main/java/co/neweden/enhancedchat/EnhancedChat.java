e9e63405a67c0534f9cca9cb654bad37133c27a8
aaron <AaronMcHale>
2017-05-24 22:13:40 +0100

    General file and formatting functions, Chat MOTD support

---
+++ src/main/java/co/neweden/enhancedchat/EnhancedChat.java
@@ -0,0 +1,45 @@
+package co.neweden.enhancedchat;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class EnhancedChat {
+
+    protected static Main plugin;
+
+    public static Main getPlugin() { return plugin; }
+
+    public static Logger getLogger() { return plugin.getLogger(); }
+
+    public static Path evalPath(String path) throws InvalidPathException {
+        if (path.equals(""))
+            throw new InvalidPathException(path, "Path cannot be empty.");
+
+        if (path.contains(".."))
+            throw new InvalidPathException(path, "When trying to load the file path '" + path + "' contained '..', for security paths containing two dots are not allowed as they could be used to load files outside of the plugin folder.");
+
+        return Paths.get(getPlugin().getDataFolder().getPath() + File.separator + path);
+    }
+
+    public static BaseComponent[] safeGetFormattedFile(String filePath) throws InvalidPathException, IOException {
+        List<String> lines = Files.readAllLines(evalPath(filePath), StandardCharsets.UTF_8);
+        StringBuffer string = new StringBuffer();
+        for (String line : lines) {
+            string.append(line + '\n');
+        }
+        String formatted = ChatColor.translateAlternateColorCodes('&', string.toString());
+        return TextComponent.fromLegacyText(formatted);
+    }
+
+}
--- src/main/java/co/neweden/enhancedchat/Main.java
+++ src/main/java/co/neweden/enhancedchat/Main.java
@@ -1,9 +1,53 @@
 package co.neweden.enhancedchat;
 
+import com.google.common.io.ByteStreams;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.*;
+import java.util.logging.Level;
 
 public class Main extends Plugin {
 
+    private Configuration config;
 
+    @Override
+    public void onEnable() {
+        EnhancedChat.plugin = this;
+        load();
+        new Messages();
+    }
+
+    private void load() {
+        boolean copyAll = false;
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+            copyAll = true;
+        }
+
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(setupDefaultFile("config.yml"));
+            if (!copyAll) return;
+
+            setupDefaultFile("chat-motd.txt");
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private File setupDefaultFile(String name) throws IOException {
+        File file = new File(getDataFolder(), name);
+        if (!file.exists()) {
+            try (InputStream is = getResourceAsStream(name);
+                 OutputStream os = new FileOutputStream(file)) {
+                ByteStreams.copy(is, os);
+            }
+        }
+        return file;
+    }
+
+    public Configuration getConfig() { return config; }
 
 }
