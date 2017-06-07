0cda1fe8d84333ac669b268056ba16db9d7a0cb1
aaron <AaronMcHale>
2017-06-07 20:30:33 +0100

    Import cleanup

--- src/main/java/co/neweden/enhancedchat/Messages.java
+++ src/main/java/co/neweden/enhancedchat/Messages.java
@@ -6,7 +6,6 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.api.scheduler.ScheduledTask;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.event.EventHandler;
 
