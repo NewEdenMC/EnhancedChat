4bd5a1ab6d64115aaed20647b2caba581a93c3ca
aaron <AaronMcHale>
2017-11-18 11:59:13 +0000

    Added Emote command, /emote /me

--- src/main/java/co/neweden/enhancedchat/chat/ChatManager.java
+++ src/main/java/co/neweden/enhancedchat/chat/ChatManager.java
@@ -32,7 +32,7 @@ public class ChatManager {
         if (eventHandler != null)
             ProxyServer.getInstance().getPluginManager().unregisterListener(eventHandler);
         eventHandler = new EventListener();
-        new ChannelCommands();
+        new ChannelCommands(); new EmoteCommand();
     }
 
     public static Collection<Channel> getChannels() { return Collections.unmodifiableCollection(channels.values()); }
---
+++ src/main/java/co/neweden/enhancedchat/chat/EmoteCommand.java
@@ -0,0 +1,39 @@
+package co.neweden.enhancedchat.chat;
+
+import co.neweden.enhancedchat.EnhancedChat;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class EmoteCommand extends Command {
+
+    EmoteCommand() {
+        super("emote", null, "me");
+        ProxyServer.getInstance().getPluginManager().registerCommand(EnhancedChat.getPlugin(), this);
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof ProxiedPlayer)) {
+            sender.sendMessage(new ComponentBuilder("Only Players can run channel commands.").color(ChatColor.RED).create()); return;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(new ComponentBuilder("Youu must provide a message: /" + getName() + " <message>").color(ChatColor.RED).create()); return;
+        }
+
+        ProxiedPlayer player = (ProxiedPlayer) sender;
+
+        StringBuilder emote = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            emote.append(args[i]);
+            if (i <= args.length - 2)
+                emote.append(' ');
+        }
+
+        ChatManager.sendMessageToActiveChannel(player, MessageType.GAME_EMOTE, emote.toString());
+    }
+
+}
