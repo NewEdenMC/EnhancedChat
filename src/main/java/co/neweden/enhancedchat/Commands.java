150a71b5508789d591f6faed69aa4ba9b5d66242
aaron <AaronMcHale>
2017-12-15 19:40:55 +0000

    Refactored Admin Commands Class to match ChatCommands Class

--- src/main/java/co/neweden/enhancedchat/Commands.java
+++ src/main/java/co/neweden/enhancedchat/Commands.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class Commands extends Command {
@@ -16,17 +17,34 @@ public class Commands extends Command {
 
     public void execute(CommandSender sender, String[] args) {
         if (args.length < 1) {
-            sender.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',
-                    "Enhanced Chat Admin Commands:\n" +
-                    "&f- &breload&f: Reload the configuration and clear the cache"
-            )));
-            return;
+            help(sender); return;
         }
 
-        if (args[0].equalsIgnoreCase("reload")) {
-            EnhancedChat.getPlugin().reload();
-            sender.sendMessage(new ComponentBuilder("Config reloaded and cache cleared.").color(ChatColor.GREEN).create());
+        switch (args[0].toLowerCase()) {
+            case "reload" : reload(sender); return;
         }
+    }
+
+    private static void help(CommandSender sender) {
+        sender.sendMessage(new ComponentBuilder("Enhanced Chat Admin Sub-Commands:").color(ChatColor.WHITE).create());
+
+        String[] cmd = new String[1];
+        String[] desc = new String[1];
+
+        cmd[0] = "reload"; desc[0] = "Reload the configuration and clear the cache";
+
+        for (int i = 0; i < cmd.length; i++) {
+            sender.sendMessage(
+                    new ComponentBuilder("- ").color(ChatColor.WHITE)
+                            .append(cmd[i]).color(ChatColor.AQUA)
+                            .append(": " + desc[i]).color(ChatColor.WHITE).create()
+            );
+        }
+    }
+
+    private static void reload(CommandSender sender) {
+        EnhancedChat.getPlugin().reload();
+        sender.sendMessage(new ComponentBuilder("Config reloaded and cache cleared.").color(ChatColor.GREEN).create());
     }
 
 }
